83f025d7bed038dfbbe012dfad28e011df0bddfd
ehsan79f <ehsanf79@gmail.com>
2019-11-10 08:56:31 +0330

    !

--- components.php
+++ components.php
@@ -72,7 +72,7 @@ function projectList($conn){
 
 
         $output.= "<div class='col-md-12 col-lg-6'>
-                    <div id='card' class='card mb-4 box-shadow' onclick=\"location.href='project?id=".$row['_id']."';\">
+                    <div id='card' class='card mb-4 box-shadow' onclick=\"location.href='project.php?id=".$row['_id']."';\">
                         <img class='card-img-top' src='images/profiles/".$row["picture"]."' alt='Card image cap'>
                        
                         <div class='card-body'>
@@ -157,22 +157,3 @@ function fa_number($number)
     $en = array("0","1","2","3","4","5","6","7","8","9");
     $fa = array("۰","۱","۲","۳","۴","۵","۶","۷","۸","۹");
     return str_replace($en, $fa, $number);}
-function modals(){
-    return" <!-- Modal -->
-  <div class=\"modal fade\" id=\"myModal\" role=\"dialog\">
-    <div class=\"modal-dialog modal-lg\">
-      <div class=\"modal-content\">
-        <div class=\"modal-header\">
-          <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>
-          <h4 class=\"modal-title\">Modal Header</h4>
-        </div>
-        <div class=\"modal-body\">
-          <p>This is a large modal.</p>
-        </div>
-        <div class=\"modal-footer\">
-          <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>";
-}
